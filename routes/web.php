a3a0666768f5ba88e4d8361affe32c2266f1c4a8
Devprecious <oladelep77@gmail.com>
2021-11-24 15:48:14 -0800

    Page

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return route('login');
 });
 
 Route::get('/callback', [FundController::class, 'callback'])->name('callback');
